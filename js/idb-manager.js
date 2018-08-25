8da3eae509c3fe44a7a8e136fd0cd842e4fcc62a
Mattia Collalti <collalti.mattia@gmail.com>
2018-08-25 14:54:13 +0200

    feat: add features to IDBManager
    create class, init method and helpers for various operations

--- js/idb-manager.js
+++ js/idb-manager.js
@@ -1,8 +1,62 @@
-const _dbPromise = idb.open('resto-reviews-db', 1, (db) => {
-  switch (db.oldVersion) {
-    case 0:
-      const store = db.createObjectStore('restaurants', { keyPath: 'id' });
-      store.createIndex('resto-name', 'name');
-      break;
+let DBPromise;
+class IDBManager {
+  static init() {
+    DBPromise = idb.open('resto-reviews-db', 2, (db) => {
+      let store;
+      switch (db.oldVersion) {
+        case 0:
+          store = db.createObjectStore('restaurants', {keyPath: 'id'});
+          store.createIndex('resto-name', 'name');
+        case 1:
+          store = db.createObjectStore('reviews', {keyPath: 'id'});
+          store.createIndex('resto', 'restaurant_id');
+          store = db.createObjectStore(
+              'reviews-to-send', {keyPath: 'local_id', autoIncrement: true});
+          store.createIndex('resto', 'restaurant_id');
+      }
+    });
   }
-});
+  static getFromIDB(storeName, restaurant_id) {
+    if(typeof(restaurant_id) === 'string')
+      restaurant_id = parseInt(restaurant_id);
+    return DBPromise.then(db => {
+        if(!db) return;
+        return db.transaction(storeName)
+        .objectStore(storeName)
+        .get(restaurant_id);
+      });
+  }
+  static putInIDBStore(storeName, obj){
+    return DBPromise.then(db => {
+      if(!db) return;
+      const tx = db.transaction(storeName, 'readwrite');
+      tx.objectStore(storeName).put(obj);
+      return tx.complete;
+    });
+  }
+  static getTableFromIDB(storeName){
+    return DBPromise.then(db => {
+      if(!db) return;
+      const tx = db.transaction(storeName);
+      return tx.objectStore(storeName).getAll();
+    });
+  }
+  static getValueOnIndex(indexName, storeName, selector){
+    return DBPromise.then(db => {
+      return db.transaction(storeName)
+      .objectStore(storeName)
+      .index(indexName)
+      .getAll(selector);
+    });
+  }
+  static deleteFromStore(storeName, item){
+    return DBPromise.then(db => {
+      if(!db) return;
+      return db.transaction(storeName, 'readwrite')
+      .objectStore(storeName)
+      .delete(item);
+    });
+  }
+}
+
+IDBManager.init();
